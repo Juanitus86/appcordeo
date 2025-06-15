92316edd6e7fcef0ca51ef030c54a8b1459698df
Juanitus86 <oniricavida@gmail.com>
2025-06-15 13:52:41 -0500

    Update appcor.py

--- appcor.py
+++ appcor.py
@@ -1,6 +1,3 @@
-import streamlit as st
-import pandas as pd
-import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime, timedelta
 from gspread_dataframe import get_as_dataframe
