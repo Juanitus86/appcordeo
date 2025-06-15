1cded943e6c2932a529d57c0cdbbd7f1453c509a
Juanitus86 <oniricavida@gmail.com>
2025-06-15 13:51:34 -0500

    Update appcor.py

--- appcor.py
+++ appcor.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime, timedelta
